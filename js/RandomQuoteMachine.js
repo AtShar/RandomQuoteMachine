0d1a826015f504b76d2fcb7c579f5c6070971af9
Atul Sharma <atulsharma3108@gmail.com>
2017-05-02 16:54:53 +0900

    Update RandomQuoteMachine.js
    Improved Code

--- js/RandomQuoteMachine.js
+++ js/RandomQuoteMachine.js
@@ -1,59 +1,91 @@
-$(document).ready(function() {
+(function(window, document, $, undefined) {
 
-  setbackground();
-  fetchQuoteData();
+	'use strict';
 
-  $("#postTwitter").click(function() {
-    $("#postTwitter").attr("href", "https://twitter.com/intent/tweet?text=" + $("#quoteContent").html());
-  });
+	window.randomQuoteMachine = {};
 
-  $("#newQuote").click(function() {
+	randomQuoteMachine.init = function() {
 
-    fetchQuoteData();
-    setbackground();
-  });
+		// developers can access this
+		randomQuoteMachine.setbackground();
+		randomQuoteMachine.fetchQuoteData();
 
-});
+	}
 
-function fetchQuoteData() {
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+	randomQuoteMachine.twitterClick = function() {
+		$("#postTwitter").attr(
+				"href",
+				"https://twitter.com/intent/tweet?text="
+						+ $("#quoteContent").html());
+	}
 
-    $("#quoteContent").html(json["quoteText"]);
-    $("#quoteAuthor").text("-" + json["quoteAuthor"]);
-  });
-}
+	randomQuoteMachine.newQuoteClick = function() {
+		randomQuoteMachine.fetchQuoteData();
+		randomQuoteMachine.setbackground();
+	}
 
-function setbackground() {
+	randomQuoteMachine.fetchQuoteData = function() {
+		//Change url 
+		$.ajaxSetup({
+			url : "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en",
+			dataType : 'jsonp',
+			success : function(json) {
+				$("#quoteContent").html(json["quoteText"]);
+				$("#quoteAuthor").text("-" + json["quoteAuthor"]);
 
-  var index = Math.round(Math.random() * 9);
+			}
+		});
+		$.ajax();
+	}
 
-  var ColorValue = "A9A9A9"; // default color - white (index = 0)
+	randomQuoteMachine.setbackground = function() {
 
-  if (index == 1)
-    ColorValue = "708090";
-  if (index == 2)
-    ColorValue = "EE82EE";
-  if (index == 3)
-    ColorValue = "708090";
-  if (index == 4)
-    ColorValue = "6A5ACD";
-  if (index == 5)
-    ColorValue = "00FA9A";
-  if (index == 6)
-    ColorValue = "3CB371";
-  if (index == 7)
-    ColorValue = "20B2AA";
-  if (index == 8)
-    ColorValue = "2F4F4F";
-  if (index == 9)
-    ColorValue = "CCCCCC";
+		var index = Math.round(Math.random() * 9);
 
-  $("#mainContainer").css('background-color', "#" + ColorValue);
-  $("#twitterContainer").css('background-color', "#" + ColorValue);
-  $("#newQuote").css('background-color', "#" + ColorValue);
+		var ColorValue = "A9A9A9"; // default color - white (index = 0)
 
-  $(".fa-quote-right").css('color', "#" + ColorValue);
-  $(".fa-quote-left").css('color', "#" + ColorValue);
-  $("#quoteContent").css('color', "#" + ColorValue);
-  $("#quoteAuthor").css('color', "#" + ColorValue);
-}
+		if (index == 1)
+			ColorValue = "708090";
+		if (index == 2)
+			ColorValue = "EE82EE";
+		if (index == 3)
+			ColorValue = "708090";
+		if (index == 4)
+			ColorValue = "6A5ACD";
+		if (index == 5)
+			ColorValue = "00FA9A";
+		if (index == 6)
+			ColorValue = "3CB371";
+		if (index == 7)
+			ColorValue = "20B2AA";
+		if (index == 8)
+			ColorValue = "2F4F4F";
+		if (index == 9)
+			ColorValue = "CCCCCC";
+
+		$("#mainContainer").css('background-color', "#" + ColorValue);
+		$("#twitterContainer").css('background-color', "#" + ColorValue);
+		$("#newQuote").css('background-color', "#" + ColorValue);
+
+		$(".fa-quote-right").css('color', "#" + ColorValue);
+		$(".fa-quote-left").css('color', "#" + ColorValue);
+		$("#quoteContent").css('color', "#" + ColorValue);
+		$("#quoteAuthor").css('color', "#" + ColorValue);
+
+	}
+	
+	$(function() {
+
+		// The DOM is ready!
+		randomQuoteMachine.init();
+		$("#postTwitter").on('click', randomQuoteMachine.twitterClick);
+
+	    $("#newQuote").on('click', randomQuoteMachine.newQuoteClick);
+
+	});
+	
+	
+
+	
+
+})(window, document, jQuery);
